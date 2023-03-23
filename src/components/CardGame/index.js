d521b0ab639f5c8e1b02a9a2ee7b1673e33af835
Bruno Leite <bruunopereira19@gmail.com>
2023-03-23 17:22:58 -0300

    initial commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const CardGame = () =>{
+  return`
+    <article class='card-game'>
+      <img src="./src/components/images/alura-pixel.png" alt="logo alura">
+    </article>
+  
+  `
+}
+
+export default CardGame;
