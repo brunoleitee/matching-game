f0d59cf0eaf8af8680f4bfa4abf539ff30586ae9
Bruno Leite <bruunopereira19@gmail.com>
2023-03-23 22:37:19 -0300

    :memo: inserindo reset css
    inserindo reset global css

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import CardGame from "./src/components/CardGame"
+import "./src/styles/settings/colors.css"
+import "./src/styles/elements/base.css"
+import "./src/styles/generic/reset.css"
 
 const $root = document.querySelector('#root')
 const $htmlCardGame = CardGame()
